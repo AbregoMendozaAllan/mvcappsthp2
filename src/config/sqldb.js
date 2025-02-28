8c81d311bbaa80ace301c9eb50cfee50e2bad605
AbregoMendozaAllan <153949305+AbregoMendozaAllan@users.noreply.github.com>
2025-02-28 00:38:42 -0600

    updated sqldb to use process.env

--- src/config/sqldb.js
+++ src/config/sqldb.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql';
 
 const sqldb = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '@M3ndoZ1991MApass',
-    database: 'seminario',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 })
 
 sqldb.connect((error)=>{
